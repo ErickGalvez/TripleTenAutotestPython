f8dd1e12caddea9584235e45cc251637ea01c9e6
ErickGalvez <62743274+ErickGalvez@users.noreply.github.com>
2025-01-20 16:47:39 -0600

    Terminar tests de kit
    se agregaron todos los assert necesarios para automatizar la prueba, asi como funciones especiales para las pruebas 8 y 9

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = 'https://cnt-27eec0b1-6cb0-4e10-a008-b62d26f8c29e.containerhub.tripleten-services.com' # Inserta tu dirección de URL sin la barra diagonal al final)
+URL_SERVICE = 'https://cnt-c9c3457e-99a0-4860-952a-ea2d7cf0c736.containerhub.tripleten-services.com' # Inserta tu dirección de URL sin la barra diagonal al final)
 CREATE_USER_PATH = "/api/v1/users" #Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" #Almacena la ruta para crear un kit en esta variable
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -7,9 +7,6 @@ import configuration
 kit_body = data.kit_body.copy()
 user_body = data.user_body.copy()
 
-
-
-
 def create_user(body):
     request = requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,"",body)
     print(request.json())
@@ -26,8 +23,53 @@ def get_token(request):
     return auth_token
 
 def create_kit(kit_name):
+    print("creating kit with kit name:" + kit_name["name"])
     token = get_token(create_user(user_body))
+    print("token is "+ token["Authorization"])
     response_kit = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,json = kit_name,headers = token)
     return response_kit
 
-print(create_kit(kit_body).json())
+def change_kit_body(kit_name):
+    print("current default kit name: " + kit_body["name"])
+    print("changing to " + kit_name)
+    kit_body["name"] = kit_name
+    return kit_body
+
+def kit(kit_string):
+    kit_name = change_kit_body(kit_string)
+    response = create_kit(kit_name)
+    return response.status_code
+
+def create_empty_kit(): #special function to insert empty json as parameter
+    token = get_token(create_user(user_body))
+    response_kit = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,json = {},headers = token)
+    return response_kit
+
+def create_number_kit(): #special function to insert int number in "name"
+    token = get_token(create_user(user_body))
+    response_kit = requests.post(configuration.URL_SERVICE + configuration.KITS_PATH, json={"name" : 123}, headers=token)
+    return response_kit
+
+def positive_assert(name):
+    assert kit(name) == 201
+def negative_assert(name):
+    assert kit(name) == 400
+
+def test_two_letter_kit_name():
+    positive_assert("aa")
+def test_five_one_one_letter_kit_name():
+    positive_assert(data.name_five_one_one)
+def test_empty_letter_kit_name():
+    negative_assert("")
+def test_five_one_two_letter_kit_name():
+    negative_assert(data.name_five_one_two)
+def test_special_character_kit_name():
+    positive_assert("\"№%@\",")
+def test_space_kit():
+    positive_assert("A Aaa ")
+def test_number_kit_name():
+    positive_assert("123")
+def test_no_name():
+    assert create_empty_kit().status_code == 400
+def test_int_name():
+    assert create_number_kit().status_code == 400
